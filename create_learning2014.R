0b985e56af8be063d93de9c9042967ee8c3b1bd0
yufanyin <57188877+yufanyin@users.noreply.github.com>
2019-11-11 23:46:26 +0200

    Add files via upload
    for Exercise 2

---
+++ create_learning2014.R
@@ -0,0 +1,20 @@
+# Exercise 2 Data wrangling
+# Yufan
+# 11.11.2019
+# reference to the data source:
+# https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt
+
+# read the data into memory
+lrn14 <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=JYTOPKYS3-data)
+
+# Look at the dimensions of the data
+dim(lrn14)
+
+# Look at the structure of the data
+str(lrn14)
+
+# [1] 183  60
+
+# 'data.frame':	183 obs. of  60 variables:
+# gender  : Factor w/ 2 levels "F","M"
+# other variables (Age, Attitude, Points, Aa, ... , ST01, ...): int
